cdd7294e3a5acf8789b25abf98d5baf9fb8f328a
tygutowski <tygutowski@gmail.com>
2022-09-14 10:51:04 -0400

    Grabs input and slices(?) it before printing

--- project-1/hello.go
+++ project-1/hello.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) < 2 {
+		fmt.Println("Missing parameter, provide file name!")
+		return
+	}
+	data, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println("Cannot read file:", os.Args[1])
+		panic(err)
+	}
+	for _, line := range data.slice {
+		fmt.Println(line)
+	}
 }
