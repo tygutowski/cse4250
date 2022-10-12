5132791a4244675734e3a99b6db47734d3fd72c4
B-Megamaths <101736640+B-Megamaths@users.noreply.github.com>
2022-10-12 18:26:11 -0400

    Added sorting function and squashed some bugs

--- project-2/src/main.rs
+++ project-2/src/main.rs
@@ -7,9 +7,9 @@ pub struct Manatee {
 }
 
 fn main() -> io::Result<()> {
-	let mut pairs: i32;
-	let mut female_manatees: Vec<Manatee>;
-	let mut male_manatees: Vec<Manatee>;
+	let mut pairs: usize = 0;
+	let mut female_manatees: Vec<Manatee> = Vec::new();
+	let mut male_manatees: Vec<Manatee> = Vec::new();
 	
 	// Read the first five lines from stdio
 	for n in 1..6  {
@@ -17,33 +17,74 @@ fn main() -> io::Result<()> {
 		let mut buffer = String::new();
 		// Set the next input equal to the input variable
 		io::stdin().read_line(&mut buffer)?;
-		
+		let mut buffer_split = buffer.split_whitespace();
 		
 		match n {
 			// Get number of pairs
-			1 => pairs = n,
+			1 => pairs = buffer_split.next().expect("found empty string where number expected").parse::<usize>().unwrap(),
 			
 			// Get female manatee age
 			2 => 
 			for i in 1..pairs {
-				female_manatees[i].age = buffer[i].parse::<i32>().unwrap();
+				let ind: i32 = i as i32;
+				let empty_manatee = Manatee {
+					size: 0,
+					age: 0,
+					index: ind
+				};
+				female_manatees.push(empty_manatee);
+				female_manatees[i].age = buffer_split.next().expect("found empty string where number expected").parse::<i32>().unwrap();
 			},
 			// Get female manatee size
 			3 =>
 			for i in 1..pairs {
-				female_manatees[n].size = buffer[i].parse::<i32>().unwrap();
+				female_manatees[i].size = buffer_split.next().expect("found empty string where number expected").parse::<i32>().unwrap();
 			},
 			// Get male manatee age
 			4 =>
 			for i in 1..pairs {
-				male_manatees[n].age = buffer[i].parse::<i32>().unwrap();
+				let ind: i32 = i as i32;
+				let empty_manatee = Manatee {
+					size: 0,
+					age: 0,
+					index: ind
+				};
+				male_manatees.push(empty_manatee);
+				male_manatees[i].age = buffer_split.next().expect("found empty string where number expected").parse::<i32>().unwrap();
 			},
 			// Get male manatee size
 			5 => 
 			for i in 1..pairs {
-				male_manatees[n].size = buffer[i].parse::<i32>().unwrap();
+				male_manatees[i].size = buffer_split.next().expect("found empty string where number expected").parse::<i32>().unwrap();
 			},
 		}			
 	}
+
+	// Now that we have the trees, sort by age
+    // First the female manatees...
+    for j in 1..(pairs - 1) {
+    	let mut swap_index = j;
+		for n in j..pairs {
+			if female_manatees[n].age <= female_manatees[swap_index].age {
+				swap_index = n;
+			}
+		}
+        let temp = female_manatees[j];
+		female_manatees[j] = female_manatees[swap_index];
+		female_manatees[swap_index] = temp;
+	}
+	// then the male ones
+	for j in 1..(pairs - 1) {
+    	let mut swap_index = j;
+		for n in j..pairs {
+			if male_manatees[n].age <= male_manatees[swap_index].age {
+				swap_index = n;
+			}
+		}
+        let temp = male_manatees[j];
+		male_manatees[j] = male_manatees[swap_index];
+		male_manatees[swap_index] = temp;
+	}
+
 	Ok(())
 }
