dd37e32d975404027608dd2d096e5c11d6a8b2e8
tygutowski <tygutowski@gmail.com>
2022-10-12 20:14:43 -0400

    trying a new sort method

--- project-2/target/debug/deps/project_2.d
+++ project-2/target/debug/deps/project_2.d
@@ -1,5 +1,5 @@
-C:\Users\tygut\Documents\Projects\CSE4250\cse4250\project-2\target\debug\deps\project_2.exe: src\main.rs
+C:\Users\tygut\Documents\cse4250\project-2\target\debug\deps\project_2.exe: src\main.rs
 
-C:\Users\tygut\Documents\Projects\CSE4250\cse4250\project-2\target\debug\deps\project_2.d: src\main.rs
+C:\Users\tygut\Documents\cse4250\project-2\target\debug\deps\project_2.d: src\main.rs
 
 src\main.rs:
