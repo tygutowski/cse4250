8b28e7013bfabb5508cba7ec6891092ee840b781
B-Megamaths <101736640+B-Megamaths@users.noreply.github.com>
2022-10-14 18:56:40 -0400

    Implemented sorting algorithm

--- project-2/src/main.rs
+++ project-2/src/main.rs
@@ -25,8 +25,8 @@ fn main() -> io::Result<()> {
 	let mut pairs: usize = 0;
 	let mut female_manatees: Vec<Manatee> = Vec::new();
 	let mut male_manatees: Vec<Manatee> = Vec::new();
-	let mut max_female_size: i32 = 0;
-	let mut max_male_size: i32 = 0;
+	// let mut max_female_size: i32 = 0; // Seems unused?
+	// let mut max_male_size: i32 = 0; // Also seems unused
 	// Read the first five lines from stdio
 	for n in 1..6  {
 		// Set the input variable to be a new string
@@ -93,30 +93,67 @@ fn main() -> io::Result<()> {
 
 	
 	// Sort both vectors of manatees.
-
-	
-	let mut impossible: bool = true;
 	
 	let mut female_answer: Vec<i32> = Vec::new();
 	let mut male_answer: Vec<i32> = Vec::new();
 	
 	// morbin' time.
 
+	// Assume no pair found until one is found
+	let mut impossible = false;
+	let mut no_pair = true;
+
 	for i in 0..pairs-1 {
 		// Sort both lists of manatees
 		female_manatees.sort_by(|a, b| a.cmp(b));
 		male_manatees.sort_by(|a, b| a.cmp(b));
 		// Get the first manatee in each list
-		let mut first_male = &male_manatees[0];
-		let mut first_female = &female_manatees[0];
+		let first_male = male_manatees[i];
+		let first_female = female_manatees[i];
 		// Get the smallest (first) female that is larger than the male.
-		if(female_manatees[i].size > first_male.size) {
-			print!("{}:{}", female_manatees[i].age, female_manatees[i].size);
-			female_answer.push(female_manatees[i].index);
-			male_answer.push(male_manatees[i].index);
-			break;
+		let youngest = first_female.age;
+		for j in i..pairs {
+			if(female_manatees[j].size > first_male.size) {
+			   	print!("{}:{}", female_manatees[j].age, female_manatees[i].size); // Debug line; remove later
+				female_answer.push(female_manatees[j].index);
+				male_answer.push(male_manatees[i].index);
+				no_pair = false;
+				break;
+			}
+			// If you've passed the correct manatee age range, break off
+			if(female_manatees[j].age > youngest) {
+				break;
+			}
 		}
-	}
+		if(no_pair) {
+			// No pair? This manatee might match up with an older manatee.
+			// Find the best match for the female, instead.
+            let mut best_match = first_male;
+			let youngest = first_male.age;
+			// Finding the best male for the first female is a bit more complex.
+			// We want the LARGEST one, but we don't know which is the largest
+			// until we've polled them all (of the right age).
+			for j in i..pairs {
+				if((best_match.size < male_manatees[j].size && male_manatees[j].size < first_female.size) || best_match.size > first_female.size) {
+                    best_match = male_manatees[j];
+				}
+				// Once we find one that's too big or too old, break off.
+				if(male_manatees[j].age > youngest || best_match.size > first_female.size) {
+					break;
+				}
+			}
+
+			if(best_match.size > first_female.size) {
+				// There's no good match for this female.
+				// The setup is impossible.
+				impossible = true;
+				break;
+			}
+			// Otherwise, add the pair.
+			female_answer.push(female_manatees[i].index);
+			male_answer.push(best_match.index);
+		}
+	} 
 	//if impossible {
 	//	print!("impossible");	
 	//}
@@ -125,7 +162,7 @@ fn main() -> io::Result<()> {
 	//let mut manatee_index = male_manatees.binary_search_by_key(&11, |&Manatee{ size, age, index }| age).unwrap();
 	//print!("{}", male_manatees[manatee_index].size);
 	
-	// print indeces of manatees
+	// print indices of manatees
 	print!("\n");
 	for i in 0..(pairs) {
 		let current_manatee = female_answer[i];
